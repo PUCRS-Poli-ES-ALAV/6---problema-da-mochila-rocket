cb1664fe6d1055155160ae84535b0f04483d73f4
Giovanna Castro <gihcastro1010@gmail.com>
2025-04-06 23:29:14 -0300

    feat(BackpackProblem): implement initial backpack problem setup and item class

---
+++ BackpackProblem.java
@@ -0,0 +1,12 @@
+public class BackpackProblem {
+    public static void main(String[] args) {
+        int N = 4;
+        Item[] itens = new Item[N + 1];
+        itens[1] = new Item(5, 2);
+        itens[2] = new Item(2, 4);
+        itens[3] = new Item(2, 2);
+        itens[4] = new Item(1, 3);
+    }
+
+    
+}
---
+++ Item.java
@@ -0,0 +1,9 @@
+public class Item {
+    public int peso;
+    public int valor;
+
+    public Item(int peso, int valor){
+        this.peso = peso;
+        this.valor = valor;
+    }
+}
