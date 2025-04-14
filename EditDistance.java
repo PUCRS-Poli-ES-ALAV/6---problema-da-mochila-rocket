1948f82aa5e6dd7663c7091889fa88cf42b9786a
Gio <132241659+gihcst@users.noreply.github.com>
2025-04-14 23:34:50 +0000

    fix(EditDistance): refactor main method and improve edge case handling

--- EditDistance.java
+++ EditDistance.java
@@ -1,14 +1,20 @@
 public class EditDistance {
+    public static void main(String[] args) {
+        String s1 = "Casablanca";
+        String s2 = "Portentoso";
+
+        System.out.println(ed(s1, s2, s1.length(), s2.length()));
+    }
 
     public static int ed(String SInicial, String TFinal, int i, int j) {
-        if (SInicial.length() == 0 ) { // se um dos strings estiver vazio, a distância é o comprimento do outro string
+        if (i == 0 ) { // se um dos strings estiver vazio, a distância é o comprimento do outro string
             return j;
-        } else if (TFinal.length() == 0) {
+        } else if (j == 0) {
             return i;
-        } else if (SInicial.length() == 0 && TFinal.length() == 0) { // caso as duas strings estiver vazias, a distância é 0
+        } else if (i == 0 && j == 0) { // caso as duas strings estiver vazias, a distância é 0
             return 0;
         
-        } else if (SInicial.charAt(i) == TFinal.charAt(j)) { // se os últimos caracteres forem iguais, não precisamos fazer nada
+        } else if (SInicial.charAt(i-1) == TFinal.charAt(j-1)) { // se os últimos caracteres forem iguais, não precisamos fazer nada
             return ed(SInicial, TFinal, i - 1, j - 1);
         } else { 
             int replace = ed(SInicial, TFinal, i - 1, j - 1); // substituir o último caractere de SInicial pelo último caractere de TFinal
@@ -17,4 +23,7 @@ public class EditDistance {
             return Math.min(replace, Math.min(insert, delete)) + 1;
         }
     }
+
+    
+
 }
